2a28369f18e6ef4984e24bdafe9afc188ded5afe
Arthur Habicht <arthur.habicht@ti8m.ch>
2018-04-26 20:31:57 +0200

    test

---
+++ reservation-service/src/main/java/com/example/reservationservice/ReservationServiceApplication.java
@@ -0,0 +1,112 @@
+package com.example.reservationservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.stream.Stream;
+
+@SpringBootApplication
+public class ReservationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationServiceApplication.class, args);
+    }
+}
+
+@RestController
+class MessageRestController {
+
+    private final String value;
+
+    @Autowired
+    public MessageRestController(@Value("${message}") String value) {
+        this.value = value;
+    }
+
+    @RequestMapping (method = RequestMethod.GET, value = "/message")
+    String read() {
+        return this.value;
+    }
+}
+
+
+@Component
+class SampleDataCLR implements CommandLineRunner { //CommandLineRunner for Initialization
+
+
+    private final ReservationRepository reservationRepository;
+
+    @Autowired
+    public SampleDataCLR(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+
+        Stream.of("Josh", "Juergen", "a", "b", "c", "d").forEach(name -> reservationRepository.save(new Reservation(name)));
+        reservationRepository.findAll().forEach(System.out::println);
+    }
+}
+
+
+@RepositoryRestResource
+interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+}
+
+@Entity
+class Reservation {
+
+    public Reservation(String reservationName) {
+        this.reservationName = reservationName;
+    }
+
+    public Reservation() {
+
+    }
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String reservationName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getReservationName() {
+        return reservationName;
+    }
+
+    public void setReservationName(String reservationName) {
+        this.reservationName = reservationName;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Reservation{" +
+                "id=" + id +
+                ", reservationName='" + reservationName + '\'' +
+                '}';
+    }
+}
